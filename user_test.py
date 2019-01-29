badfd9ab7c6e8926509cc7a329759562a1039a83
kennymaina <victormurigi@outlook.com>
2019-01-29 08:16:50 +0300

    setUp

--- user_test.py
+++ user_test.py
@@ -11,11 +11,11 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_user = User("kenny","kenny","12345ke","kenny@moringaschool.com")
+    # def setUp(self):
+    #     '''
+    #     Set up method to run before each test cases.
+    #     '''
+    #     self.new_user = User("kenny","kenny","12345ke","kenny@moringaschool.com")
 
     
 
